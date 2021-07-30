6630c3c48d6050f54e1ffe1070f94b8796b88daa
TranHieu1104 <ayatosama246@gmail.com>
2021-07-30 21:32:33 +0700

    excercise1.js

---
+++ excercise1.js.js
@@ -0,0 +1,54 @@
+// Khai báo 8 biến và 4 hằng theo nội dung tự chọn, liên hệ từ cuộc sống. Biến và hằng đa dạng về kiểu dữ liệu Number (2 biến số nguyên, 2 biến số thập phân), Boolean (2 biến), String (2 biến) Hằng số là những giá trị mà ko thay đổi hoặc rất hiếm khi thay đổi trong thực tế.
+var age_of_first_person = 15;
+var age_of_second_person = 10;
+var salary_of_the_first_person = 5.99$;
+var salary_of_the_second_person = 6.00$;
+let isPending = false;
+let isDone = true;
+var age = 14
+var address "Tổ 8, khu 4, Thanh Sơn, Uông Bí, Quảng Ninh";
+const MAX_AGE = 80;
+MAX_AGE = MAX_AGE + 1;
+console.log(MAX_AGE);
+
+// Thực hiện in ra kết quả các phép +, -, *, /, lấy dư với 2 số nguyên
+var x = 500
+var y = 20
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y)
+
+// Thực hiện in ra kết quả các pháp +, -, *, / với 2 số thập phân
+var a = 13.5
+var b = 4.5
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+// Thực hiện in ra nối 2 biến string.
+var message = "xin chào" + "và tạm biệt ";
+
+// Khai báo 1 biến mới và thực hiện phép gán 1 biến bất kỳ cho biến mới này. In ra giá trị của biến mới này
+var x = 10
+var z = x;
+console.log(z)
+
+// Thực hiện tăng thêm 1, giảm đi 1 đối với 1 biến là số bất kỳ. In ra kết quả.
+var c = 15
+++c;
+console.log(c)
+
+var d = 11
+--d;
+console.log(d)
+
+// Thực hiện tăng thêm 10 lần và giảm 5 lần đối với 1 biến là số bất kỳ. In ra kết quả.
+var a = 10
+a *= 10
+console.log(a)
+
+var b = 20
+b /= 4
+console.log(b)
