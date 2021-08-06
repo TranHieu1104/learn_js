cefd9c7f2511c9793a008677f75b48c49bb9298c
TranHieu1104 <ayatosama246@gmail.com>
2021-08-06 20:45:22 +0700

    excercise2.js

--- excercise2.js
+++ excercise2.js
@@ -1,42 +1,46 @@
 // Tự nghĩ ra 1 mảng trong thực tế.
-colour = [ 'red', 'green', 'black', 'blue', 'white']
-console.log(colour[0]);
-console.log(colour[1]);
-console.log(colour[2]);
-console.log(colour[3]);
-console.log(colour[4]);
+var colors = [ 'red', 'green', 'black', 'blue', 'white']
+for (var i = 0; i < colour.length; i ++) {
+  console.log(colours[i])
+}
 
 // In ra các phần tử trong mảng theo đúng thứ tự bằng 2 cách và ngược thứ tự bằng cách 1
-var colour = [ 'red', 'green', 'black', 'blue', 'white'];
-for (var i = 0; i < colour.length; i += 1) {
-    console.log(colour[i])
+var colors = [ 'red', 'green', 'black', 'blue', 'white'];
+for (var i = 0; i < colours.length; i ++) {
+    console.log(colours[i])
 }
 
-var colour = ['red', 'green', 'black ', 'blue', 'white'];
-for (var colour of colour) {
-  console.log(colour)
+var colors = ['red', 'green', 'black ', 'blue', 'white'];
+for (var color of colors) {
+  console.log(colors)
 }
 
-var colour = ['red', 'green', 'black', 'blue', 'white'];
-for (var i = 5; i >= 0; i--) {
-  console.log(colour[i])
+var colors = ['red', 'green', 'black', 'blue', 'white'];
+for (var i = colors.length; i >= 0; i--) {
+  console.log(colors[i])
 }
 
 // Thêm 1 phần tử mới vào mảng. (mới)
-colour = [ 'red', 'green', 'black', 'blue', 'white', 'purple']
+const colors = [ 'red', 'green', 'black', 'blue', 'white'];
+colors.push('purple');
+console.log(colors)
 
 // Xóa 1 phần tử trong mảng ở vị trí đầu tiên.
-colour = [ 'green', 'black', 'blue', 'white']
+const colors = [ 'red', 'green', 'black', 'blue', 'white']
+colors.shift ('red')
+console.log(colors)
 
 // Xóa 1 phần tử trong mảng ở vịt trí cuối cùng.
-colour = [ 'red', 'green', 'black', 'blue']
+const colors = [ 'red', 'green', 'black', 'blue', 'white']
+colors.pop('white')
+console.log(colors)
+
 
 // Nghĩ thêm 1 mảng khác
 OS = [ 'Windows', 'macOS', 'Linux']
 
 // Hợp nhất (merge) 2 mảng lại với nhau. In ra tất cả các phần tử ở mảng hợp nhất.
-const colour = [ 'red', 'green', 'black', 'blue', 'white'];
+const colors = [ 'red', 'green', 'black', 'blue', 'white'];
 const OS = [ 'Windows', 'macOS', 'Linux'];
-const all = colour.concat(OS);
-console.log(colour);
-console.log(OS);
+const all = colors.concat(OS);
+console.log(all);
